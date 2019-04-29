e4e9bce6935aadcd73bfbceafaa8f25fccbc5efd
justinc987 <justinc9877@gmail.com>
2019-04-29 09:38:30 -0400

    Added missing imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 
+import { MatDividerModule } from '@angular/material';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ProjectsPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, HeaderComponent, ProjectsPageComponent, HomePageComponent, FooterComponent ],
+	imports: [ BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatDividerModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
-odule { }
+export class AppModule {}
